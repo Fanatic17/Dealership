5d94c40e217541eb240faffbea0eb0e10995715c
Fanatic17 <fanaticcar_17@hotmail.it>
2017-11-05 18:03:15 +0100

    Style changes

--- SuperCoolApp/Program.cs
+++ SuperCoolApp/Program.cs
@@ -20,6 +20,7 @@ namespace SuperCoolApp
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>().UseUrls("http://localhost:56000")
+                .UseEnvironment("Development")
                 .Build();
     }
 }
